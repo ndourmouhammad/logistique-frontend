937bcccd6933ba8b8ee02d4e0980c3e10420244e
Mouhammad Ndour <ndourmouhammad15@gmail.com>
2026-07-01 19:25:30 +0000

    feat: add ngrok interceptor to bypass browser warnings in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
+import { ngrokInterceptor } from './core/interceptors/ngrok.interceptor';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor, ngrokInterceptor])),
     provideRouter(routes)
   ]
 };
